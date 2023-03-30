5ece7bce4cf99d770b8f9fb1e271bcde6aa5b078
raluido <raluido@gmail.com>
2023-03-30 13:16:55 +0100

    working on checking already read messages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Models\User;
+use App\Models\Message;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -48,6 +49,33 @@ class HomeController extends Controller
             ->where('messages.id', $id)
             ->get();
 
+        if (empty($replies[0])) {
+            $message = Message::find($id);
+            $message->read = true;
+            $message->save();
+        } else {
+            Db::table('replies')
+                ->where('id', $id)
+                ->orderBy('updated_at', 'DESC')
+                ->update(['read' => true]);
+        }
+
         return view('user.message', compact('nick', 'id', 'messages', 'replies'));
+    }
+
+    public function checkMessages()
+    {
+        $id = auth()->id();
+
+        $messages = Db::table('messages')
+            ->where('sender_id', $id)
+            ->orWhere('receiver_id', $id)
+            ->get();
+
+        $replies = Db::table('messages')
+            ->select('messages.id', 'replies.content_reply', 'replies.sender_id_reply')
+            ->join('replies', 'messages.id', '=', 'replies.message_id')
+            ->where('messages.id', $id)
+            ->get();
     }
 }
