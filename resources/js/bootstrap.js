03d59bdf081f4e7cbd3dfcb55da713d50c6a08a3
raluido <raluido@gmail.com>
2023-11-02 21:42:26 +0000

    change bootstrap configuration for echo

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,9 +28,10 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    encrypted: false,
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    forceTLS: false,
     enabledTransports: ['ws', 'wss'],
 });
 
